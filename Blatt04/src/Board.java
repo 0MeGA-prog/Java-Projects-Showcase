a063948395d918cf3ea86b95c827273ef73edb74
Alexej Rechter <77469252+0MeGA-prog@users.noreply.github.com>
2025-11-11 20:49:34 +0100

    Add files via upload

---
+++ Blatt04/src/Board.java
@@ -0,0 +1,179 @@
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Stack;
+
+import static java.lang.Math.abs;
+/**
+ * This class represents a generic TicTacToe game board.
+ */
+public class Board {
+    private int n;
+    //TODO
+    private int[][] board;
+    private int freeFields;
+
+    /**
+     *  Creates Board object, am game board of size n * n with 1<=n<=10.
+     */
+    public Board(int n) throws InputMismatchException
+    {
+        // TODO
+        this.n = n;
+        if (1 > this.n || this.n > 10) {
+            throw new InputMismatchException();
+        }
+        this.freeFields = n * n;
+        this.board = new int[n][n];
+    }
+    
+    /**
+     *  @return     length/width of the Board object
+     */
+    public int getN() { return n; }
+    
+    /**
+     *  @return     number of currently free fields
+     */
+    public int nFreeFields() {
+        // TODO
+        return this.freeFields;
+    }
+    
+    /**
+     *  @return     token at position pos
+     */
+    public int getField(Position pos) throws InputMismatchException
+    {
+        // TODO
+        if (pos.x < 0 || pos.y < 0 || pos.x >= this.n || pos.y >= this.n) {
+            throw new InputMismatchException();
+        }
+        return this.board[pos.x][pos.y];
+    }
+
+    /**
+     *  Sets the specified token at Position pos.
+     */    
+    public void setField(Position pos, int token) throws InputMismatchException
+    {
+        // TODO
+        if (pos.x < 0 || pos.y < 0 || pos.x >= this.n || pos.y >= this.n || (token != -1 && token != 1 && token != 0)) {
+            throw new InputMismatchException();
+        }
+        if (token != this.board[pos.x][pos.y] && token != 0 && this.board[pos.x][pos.y] == 0) {
+            this.freeFields--;
+        }
+        else if (token != this.board[pos.x][pos.y] && token == 0) {
+            this.freeFields++;
+        }
+        this.board[pos.x][pos.y] = token;
+    }
+    
+    /**
+     *  Places the token of a player at Position pos.
+     */
+    public void doMove(Position pos, int player) throws InputMismatchException
+    {
+        // TODO
+        this.setField(pos, player);
+    }
+
+    /**
+     *  Clears board at Position pos.
+     */
+    public void undoMove(Position pos) {
+        // TODO
+        this.setField(pos,0);
+    }
+    
+    /**
+     *  @return     true if game is won, false if not
+     */
+    public boolean isGameWon() {
+        // TODO
+        boolean x1 = true;
+        boolean x2 = true;
+        int comparex1 = this.board[0][0];
+        int comparex2 = this.board[this.n - 1][0];
+        if (this.board[0][0] == 0) {
+            x1 = false;
+        }
+        if (this.board[this.n - 1][0] == 0) {
+            x2 = false;
+        }
+        for (int i = 0; i < this.n; i++) {
+            boolean thisy = true;
+            int camparey = this.board[i][0];
+            if (camparey == 0) {
+                thisy = false;
+            }
+            boolean thisx = true;
+            int camparex = this.board[0][i];
+            if (camparex == 0) {
+                thisx = false;
+            }
+            for (int j = 0; j < this.n; j++) {
+                if (camparey != this.board[i][j]) {
+                    thisy = false;
+                }
+                if (camparex != this.board[j][i]) {
+                    thisx = false;
+                }
+                if (j == i && this.board[i][j] != comparex1) {
+                    x1 = false;
+                }
+                if (this.n - 1 - j == i && this.board[i][j] != comparex2) {
+                    x2 = false;
+                }
+            }
+            if (thisy || thisx) {
+                return true;
+            }
+        }
+        if (x1 || x2) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  @return     set of all free fields as some Iterable object
+     */
+    public Iterable<Position> validMoves()
+    {
+        // TODO
+        LinkedList<Position> positions = new LinkedList<Position>();
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                if (this.board[j][i] == 0) {
+                    positions.addLast(new Position(j, i));
+                }
+            }
+        }
+        return positions;
+    }
+
+    /**
+     *  Outputs current state representation of the Board object.
+     *  Practical for debugging.
+     */
+    public void print()
+    {
+        // TODO
+        System.out.println("This Board:");
+        System.out.print("y  ");
+        for (int i = 0; i < this.n; i++) {
+            System.out.print((i + 1) + "  ");
+        }
+        System.out.print("\n");
+        for (int y = 0; y < n; y++) {
+            System.out.print((y + 1) + ": ");
+            for (int x = 0; x < n; x++) {
+                System.out.print(this.board[x][y] + "  ");
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+    }
+}
+
---
+++ Blatt04/src/TicTacToe.java
@@ -0,0 +1,120 @@
+/**
+ * This class implements and evaluates game situations of a TicTacToe game.
+ */
+public class TicTacToe {
+
+    /**
+     * Returns an evaluation for player at the current board state.
+     * Arbeitet nach dem Prinzip der Alphabeta-Suche. Works with the principle of Alpha-Beta-Pruning.
+     *
+     * @param board     current Board object for game situation
+     * @param player    player who has a turn
+     * @return          rating of game situation from player's point of view
+    **/
+    public static int alphaBeta(Board board, int player)
+    {
+        // TODO
+        if (board.isGameWon()) {
+            return board.nFreeFields() + 1;
+        }
+        return alphaBeta(board, player, Integer.MIN_VALUE, Integer.MAX_VALUE, board.nFreeFields());
+    }
+
+    public static int alphaBeta(Board board, int player, int alpha, int beta, int depth) {
+        int value = Integer.MIN_VALUE;
+        for (Position pos : board.validMoves()) {
+            if (depth - 1 <= alpha) {
+                return alpha;
+            }
+            board.doMove(pos, player);
+            if (gameWon(board, pos)) {
+                value = Math.max(value, (board.nFreeFields() + 1));
+            } else if (depth == 1) {
+                value = Math.max(value, 0);
+            } else {
+                value = Math.max(value, -alphaBeta(board, -player, -beta, -alpha, depth - 1));
+            }
+            board.undoMove(pos);
+            alpha = Math.max(alpha, value);
+        }
+        return value;
+    }
+
+    public static boolean gameWon(Board board, Position pos) {
+        int compare = board.getField(pos);
+        boolean x = true;
+        boolean y = true;
+        boolean diagonal1 = false;
+        boolean diagonal2 = false;
+        if(pos.x == pos.y){
+            diagonal1 = true;
+        }
+        if (pos.x == (board.getN() - 1 - pos.y)) {
+            diagonal2 = true;
+        }
+        for (int i = 0; i < board.getN(); i++) {
+            if (board.getField(new Position(i, pos.y)) != compare) {
+                x = false;
+            }
+            if (board.getField(new Position(pos.x, i)) != compare) {
+                y = false;
+            }
+            if(pos.x == pos.y && board.getField(new Position(i, i)) != compare){
+                diagonal1 = false;
+            }
+            if (pos.x == (board.getN() - 1 - pos.y) && board.getField(new Position(i, board.getN() - 1 - i)) != compare) {
+                diagonal2 = false;
+            }
+        }
+        return (x || y || diagonal1 || diagonal2);
+    }
+
+    /**
+     * Vividly prints a rating for each currently possible move out at System.out.
+     * (from player's point of view)
+     * Uses Alpha-Beta-Pruning to rate the possible moves.
+     * formatting: See "Beispiel 1: Bewertung aller Zugmöglichkeiten" (Aufgabenblatt 4).
+     *
+     * @param board     current Board object for game situation
+     * @param player    player who has a turn
+    **/
+    public static void evaluatePossibleMoves(Board board, int player)
+    {
+        // TODO
+        if (board.isGameWon()) {
+            System.out.println("Game is over");
+            return;
+        }
+        if (player == 1) {
+            System.out.println("Evaluation for player 'x':");
+        } else {
+            System.out.println("Evaluation for player 'o':");
+        }
+        for (int y = 0; y < board.getN(); y++) {
+            for (int x = 0; x < board.getN(); x++) {
+                Position pos = new Position(x, y);
+                if (board.getField(pos) == 1) {
+                    System.out.print("x ");
+                } else if (board.getField(pos) == -1) {
+                    System.out.print("o ");
+                } else {
+                    board.doMove(pos, player);
+                    System.out.print(-alphaBeta(board, -player) + " ");
+                    board.undoMove(pos);
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Board board = new Board(4);
+        board.doMove(new Position(0, 0), 1);
+        board.doMove(new Position(0, 1), 1);
+        board.doMove(new Position(1, 0), 1);
+        board.print();
+        System.out.println(TicTacToe.alphaBeta(board, 1));
+    }
+}
+
